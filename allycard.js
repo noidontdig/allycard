b1214e7727516fda44bd08d8ad827be352e3b003
Alexandra Qin <alexandraqin@gmail.com>
2015-01-03 18:35:36 -0500

    Add success + error handling

--- allycard.js
+++ allycard.js
@@ -20,11 +20,36 @@ function allChecked () {
 
 function validate (event) {
   event.preventDefault();
-  if (allChecked()) {
-    alert('You\'re an ally!');
-  } else {
-    alert('Sorry... No ally card for you.');
+  if (!document.getElementById('group').value.length) {
+    emptyError();
+    return;
   }
+
+  if (allChecked()) {
+    success();
+  } else {
+    fail();
+  }
+}
+
+function success () {
+  var group = document.getElementById('group').value;
+  document.querySelector('.allycard span').innerText = group;
+  document.body.classList.remove('fail');
+  document.body.classList.remove('empty');
+  document.body.classList.add('success');
+}
+
+function fail () {
+  document.body.classList.remove('empty');
+  document.body.classList.remove('success');
+  document.body.classList.add('fail');
+}
+
+function emptyError () {
+  document.body.classList.remove('fail');
+  document.body.classList.remove('success');
+  document.body.classList.add('empty');
 }
 
 buildChecklist();
